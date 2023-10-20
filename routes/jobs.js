7603e3f860dca8a73d04a00cb1a0abad5972e9e9
belizario87 <leonardobelizario88@gmail.com>
2023-10-20 16:25:06 -0300

    (jobs.js)Add arquivo de rotas

---
+++ routes/jobs.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const Job = require("../models/job");
+
+router.get("/test", (req, res) => {
+  res.send("Deu certo !");
+});
+
+//add job via post
+router.post("/add", (req, res) => {
+  let { title, description, salary, company, email, newjob } = req.body;
+  //inserir dados
+  Job.create({
+    title,
+    description,
+    company,
+    salary,
+    email,
+    newjob,
+  })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
