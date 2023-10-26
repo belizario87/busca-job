d94834f1add0bbdfe435b344077f77d9fe2275e8
belizario87 <leonardobelizario88@gmail.com>
2023-10-26 03:48:44 -0300

    (jobs.js) Alteracao no import

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Job = require("../models/job");
+const Job = require("../models/Job");
 
 router.get("/test", (req, res) => {
   res.send("Deu certo !");
